ff7aaa69d9ccabcf66c887cbb53db14f9f1c57c3
Beth Rissman <beth.rissman@telepham.com>
2022-10-25 15:20:50 -0500

    updates to js cafe index.html

--- js/app.js
+++ js/app.js
@@ -1,11 +1,8 @@
 'use strict';
 console.log('hello from the js file!');
 
-
-
-
 function getUserName(){
-  let userName = prompt('What is your name?');
+  let userName = prompt('What is your party name?');
   console.log('user name data type:', typeof(userName));
   return document.write(userName);
 }
@@ -13,12 +10,12 @@ function getUserName(){
 
 function guessANumber(){
   let answer;
-  while(answer !== '7'){
-    answer = prompt('Guess a number between 1-10');
+  while(answer <== '8'){
+    answer = prompt('How many people are in your party?');
     if(answer !== '7'){
-      alert('Try again!');
+      alert('Reservations are for parties of 8 or less only.');
     } else {
-      alert('You are correct you choose 7!');
+      alert('Proceed to enter your reservation time request.');
     }
   }
 }
@@ -27,33 +24,33 @@ function guessANumber(){
 
 function getUserTime(){
   let message;
-  let time = prompt('what hour is it? In Military time.');
+  let time = prompt('What time is your reservation request?');
   if(time <= 11){
-    message = 'Good Morning';
+    message = 'Your brunch reservation is confirmed';
   }else if(time >= 12 && time <= 18){
-    message = 'Good Afternoon';
+    message = 'Your lunch reservation is confirmed';
   } else if(time > 18 && time < 24) {
-    message = 'Good Evening';
+    message = 'Your dinner reservation is confirmed';
   } else {
-    message = 'hello, I dont know your time.';
+    message = 'Please enter a valid time between 7 and 21';
   }
   return message;
 }
 
 
-function displayRating() {
-  let output = '';
-  let rating = prompt('scale of 1-5, how many stars?');
-  //for loop
-  for(let i = 0; i < rating; i++){
-    output += '<img  src=\'images/CRAIG-B.png\'  />';
-  }
-  return document.write(output);
-}
+// function displayRating() {
+//   let output = '';
+//   let rating = prompt('scale of 1-5, how many stars?');
+//   //for loop
+//   for(let i = 0; i < rating; i++){
+//     output += '<img  src=\'images/CRAIG-B.png\'  />';
+//   }
+//   return document.write(output);
+// }
 
 
 
-guessANumber();
-getUserName();
-getUserTime();
-displayRating();
+// guessANumber();
+// getUserName();
+// getUserTime();
+// displayRating();
